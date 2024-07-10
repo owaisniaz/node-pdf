a936a1e2d0f48569c750582f90eaca5cdc5d3cf8
Muhammad Owais <66405940+owaisniaz@users.noreply.github.com>
2024-07-10 11:13:33 +0500

    correct the phantom path

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get("/download-pdf", async (req, res) => {
       orientation: "landscape", // Set page orientation
       paginationOffset: 1, // Start page numbering at 1
       zoomFactor: "0.75", // Adjust zoom factor if content is too large
-      phantomPath: "./node_modules/phantomjs/bin/phantomjs",
+      phantomPath: "./node_modules/phantomjs-prebuilt/bin/phantomjs",
     };
 
     // Generate the PDF
