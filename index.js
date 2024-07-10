d5d72d55f2775ff9b3279284a70e67e0328becbf
Muhammad Owais <66405940+owaisniaz@users.noreply.github.com>
2024-07-10 11:09:48 +0500

    changes pathname

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get("/download-pdf", async (req, res) => {
       orientation: "landscape", // Set page orientation
       paginationOffset: 1, // Start page numbering at 1
       zoomFactor: "0.75", // Adjust zoom factor if content is too large
-      phantomPath: process.env.PHANTOMJS_BIN,
+      phantomPath: "./node_modules/phantomjs/bin/phantomjs",
     };
 
     // Generate the PDF
